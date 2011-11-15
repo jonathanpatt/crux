b31d1bd5cb5f419f191f6ec819453073b6fb94d0
Jonathan Patt <jonathanpatt@gmail.com>
2011-11-15 14:18:24 -0700

    Changes to View
    Add loadTemplate() method that handles all template loading and
    extracts variables into its scope.

--- lib/View.php
+++ lib/View.php
@@ -13,44 +13,51 @@ class View
     /**
      * Render a specified view
      *
-     * Note: Local variables are prefixed with two underscores to reduce the
-     * change of variable collision due to the use of the extract() function,
-     * which converts an associative array of variables to actual local
-     * variables in scope.
-     *
-     * @param string $__viewName The name of the view to render
-     * @param array  $__viewVars Optional assoc. array of variables to pass into view
-     * @param string $__layout   Optional name of the layout to wrap the view with
+     * @param string $viewName The name of the view to render
+     * @param array  $viewVars Optional assoc. array of variables to pass into view
+     * @param string $layout   Optional name of the layout to wrap the view with
      *
      * @return void
      * @author Jonathan Patt <jonathanpatt@gmail.com>
      */
-    public function render($__viewName, $__viewVars = array(), $__layout = 'default')
+    public function render($viewName, $viewVars = array(), $layout = 'default')
     {
-        $this->viewName = $__viewName;
+        $this->viewName = $viewName;
+        $this->viewVars = $viewVars;
 
-        // Import variables from the $variables array to be used in view's scope
-        // Note: This is done before importing the layout to ensure the layout can
-        // make use of these variables as well.
-        if (is_array($__viewVars)) {
-            extract($__viewVars);
-        }
-
-        // Load layout template
-        require locate_template('views/layouts/' . $__layout . '.php');
+        // Load and output layout template
+        echo $this->loadTemplate('views/layouts/' . $layout . '.php');
     }
 
     /**
-     * Load a view template and return it
+     * Load the content view template and return it
+     *
+     * This is used by the layout template to load its content.
      *
      * @return string
      * @author Jonathan Patt <jonathanpatt@gmail.com>
      */
     private function content()
     {
+        return $this->loadTemplate('views/' . $this->viewName . '.php');
+    }
+
+    /**
+     * Load a view template at the specified path, process it, and return it
+     *
+     * @return string
+     * @author Jonathan Patt <jonathanpatt@gmail.com>
+     */
+    private function loadTemplate($__path)
+    {
+        // Import variables to be used in view's scope
+        if (is_array($this->viewVars)) {
+            extract($this->viewVars);
+        }
+
         // Load and process view, then return it as a string
         ob_start();
-        require locate_template('views/' . $this->viewName . '.php');
+        require locate_template($__path);
         return ob_get_clean();
     }
 }
