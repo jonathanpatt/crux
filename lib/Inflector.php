e2bb994991e703a2ea46b506e6971c09b0b9b685
Jonathan Patt <jonathanpatt@gmail.com>
2019-12-18 08:49:41 -0700

    Replace create_function with anonymous function
    create_function() is deprecated in PHP 7.2

--- lib/Inflector.php
+++ lib/Inflector.php
@@ -7,7 +7,9 @@ class Inflector
         if (!$mixedCase) {
             $str[0] = strtoupper($str[0]);
         }
-        $func = create_function('$c', 'return strtoupper($c[1]);');
+        $func = function($c) {
+            return strtoupper($c[1]);
+        };
 
         return preg_replace_callback('/_([a-z])/', $func, $str);
     }
@@ -15,7 +17,9 @@ class Inflector
     static public function underscore($str)
     {
         $str[0] = strtolower($str[0]);
-        $func = create_function('$c', 'return "_" . strtolower($c[1]);');
+        $func = function($c) {
+            return "_" . strtolower($c[1]);
+        };
 
         return preg_replace_callback('/([A-Z])/', $func, $str);
     }
